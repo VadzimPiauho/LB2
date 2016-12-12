9394a0da55d5b948d1c9443da4d2d490806cb7f0
VadzimPliauho <vadik_pl@outlook.com>
2016-12-12 21:30:31 +0300

    12.12.2016

--- case 2/Program.cs
+++ case 2/Program.cs
@@ -10,6 +10,109 @@ namespace case_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите размерность массива");
+            try
+            {
+                Console.WriteLine("Введите М");
+                int valueM = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите N");
+                int valueN = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Неверный формат ввода размерности массива");
+                Environment.Exit(0);
+            }
+
+            int[] A = new int[5];
+            int[] B = new int[3];
+            double composAB = 1;
+            double sumA = 0;
+            double sumB = 0;
+            Random rand = new Random();
+            try
+            {
+                //double x = Math.Round(rand.NextDouble(),2)+rand.Next(100);
+                Console.WriteLine("Заполнение массива А");
+
+                for (int i = 0; i < A.Length; i++)
+                {
+                    Console.WriteLine($"ведите {i} элемент массива А");
+                    A[i] = Convert.ToDouble(Console.ReadLine());
+                    composAB = composAB * A[i];
+                    if (i % 2 == 0)
+                    {
+                        sumA += A[i];
+                    }
+                    Console.Clear();
+                }
+                for (int i = 0; i < B.GetLength(0); i++)
+                {
+                    for (int j = 0; j < B.GetLength(1); j++)
+                    {
+                        B[i, j] = Math.Round(rand.NextDouble(), 2) + rand.Next(10);
+                        composAB = composAB * B[i, j];
+                        if (j % 2 != 0)
+                        {
+                            sumB += B[i, j];
+                        }
+                    }
+                }
+                Console.WriteLine("Массив А");
+                for (int i = 0; i < A.Length; i++)
+                {
+                    Console.Write($"{A[i]}" + "\t");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Массив B");
+                for (int i = 0; i < B.GetLength(0); i++)
+                {
+                    for (int j = 0; j < B.GetLength(1); j++)
+                    {
+                        Console.Write($"{B[i, j]}" + "\t");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Максимальный элемент массива А = " + A.Max());
+                Console.WriteLine("Максимальный элемент массива B = " + B.Cast<double>().Max());
+
+                if (B.Cast<double>().Contains(A.Max()))
+                {
+                    Console.WriteLine("Максимальный общий элемент в массивах = " + A.Max());
+                }
+                else
+                {
+                    Console.WriteLine("Общего максимального элемента в массивах нет ");
+                }
+
+                Console.WriteLine("Минимальный элемент массива А = " + A.Min());
+                Console.WriteLine("Минимальный элемент массива B = " + B.Cast<double>().Min());
+
+                if (A.Min() == B.Cast<double>().Min())
+                {
+                    Console.WriteLine("Минимальный общий элемент в массивах = " + A.Min());
+                }
+                else
+                {
+                    Console.WriteLine("Общего минимального элемента в массивах нет ");
+                }
+
+                Console.WriteLine("Сумма элементов массивов = " + (A.Sum() + B.Cast<double>().Sum()));
+
+                Console.WriteLine("Общее произведение элементов массивов = " + composAB);
+
+                Console.WriteLine("Cуммa четных элементов массива А = " + sumA);
+
+                Console.WriteLine("Cуммa нечетных столбцов массива В = " + sumB);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Завершение программы");
+                Environment.Exit(0);
+            }
         }
     }
 }
--- lab2/Program.cs
+++ lab2/Program.cs
@@ -10,82 +10,95 @@ namespace case1
     {
         static void Main(string[] args)
         {
-            double[] A= new double[5];             
-            double[,] B= new double[3, 4];
+            double[] A = new double[5];
+            double[,] B = new double[3, 4];
             double composAB = 1;
             double sumA = 0;
             double sumB = 0;
             Random rand = new Random();
-          //  double x = Math.Round(rand.NextDouble(),2)+rand.Next(100);
-            Console.WriteLine("Заполнение массива А");
-            
-            for (int i=0; i<A.Length;i++  )
+            try
             {
-                Console.WriteLine($"ведите {i} элемент массива А");
-                A[i] = Convert.ToDouble(Console.ReadLine());
-                composAB = composAB*A[i];
-                if (i%2==0)
+                //double x = Math.Round(rand.NextDouble(),2)+rand.Next(100);
+                Console.WriteLine("Заполнение массива А");
+
+                for (int i = 0; i < A.Length; i++)
                 {
-                    sumA += A[i];
+                    Console.WriteLine($"ведите {i} элемент массива А");
+                    A[i] = Convert.ToDouble(Console.ReadLine());
+                    composAB = composAB * A[i];
+                    if (i % 2 == 0)
+                    {
+                        sumA += A[i];
+                    }
+                    Console.Clear();
                 }
-                Console.Clear();
-            }
-            for (int i = 0; i < B.GetLength(0); i++)
-            {
-                for (int j = 0; j < B.GetLength(1); j++)
+                for (int i = 0; i < B.GetLength(0); i++)
                 {
-                    B[i, j] = Math.Round(rand.NextDouble(), 2) + rand.Next(10);
-                    composAB = composAB * B[i, j];
+                    for (int j = 0; j < B.GetLength(1); j++)
+                    {
+                        B[i, j] = Math.Round(rand.NextDouble(), 2) + rand.Next(10);
+                        composAB = composAB * B[i, j];
+                        if (j % 2 != 0)
+                        {
+                            sumB += B[i, j];
+                        }
+                    }
                 }
-            }
-            Console.WriteLine("Массив А");
-            for (int i = 0; i < A.Length; i++)
-            {
-                Console.Write($"{A[i]}" + "\t");
-            }
-            Console.WriteLine();
-            Console.WriteLine("Массив B");
-            for (int i = 0; i < B.GetLength(0); i++)
-            {
-                for (int j = 0; j < B.GetLength(1); j++)
+                Console.WriteLine("Массив А");
+                for (int i = 0; i < A.Length; i++)
                 {
-                    Console.Write($"{B[i, j]}"+"\t");
+                    Console.Write($"{A[i]}" + "\t");
                 }
                 Console.WriteLine();
+                Console.WriteLine("Массив B");
+                for (int i = 0; i < B.GetLength(0); i++)
+                {
+                    for (int j = 0; j < B.GetLength(1); j++)
+                    {
+                        Console.Write($"{B[i, j]}" + "\t");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Максимальный элемент массива А = " + A.Max());
+                Console.WriteLine("Максимальный элемент массива B = " + B.Cast<double>().Max());
+
+                if (B.Cast<double>().Contains(A.Max()))
+                {
+                    Console.WriteLine("Максимальный общий элемент в массивах = " + A.Max());
+                }
+                else
+                {
+                    Console.WriteLine("Общего максимального элемента в массивах нет ");
+                }
+
+                Console.WriteLine("Минимальный элемент массива А = " + A.Min());
+                Console.WriteLine("Минимальный элемент массива B = " + B.Cast<double>().Min());
+
+                if (A.Min() == B.Cast<double>().Min())
+                {
+                    Console.WriteLine("Минимальный общий элемент в массивах = " + A.Min());
+                }
+                else
+                {
+                    Console.WriteLine("Общего минимального элемента в массивах нет ");
+                }
+
+                Console.WriteLine("Сумма элементов массивов = " + (A.Sum() + B.Cast<double>().Sum()));
+
+                Console.WriteLine("Общее произведение элементов массивов = " + composAB);
+
+                Console.WriteLine("Cуммa четных элементов массива А = " + sumA);
+
+                Console.WriteLine("Cуммa нечетных столбцов массива В = " + sumB);
+
             }
-            Console.WriteLine();
-
-            Console.WriteLine("Максимальный элемент массива А = " + A.Max());
-            Console.WriteLine("Максимальный элемент массива B = " + B.Cast<double>().Max());
-
-            if (B.Cast<double>().Contains(A.Max()))
+            catch (Exception)
             {
-                Console.WriteLine("Максимальный общий элемент в массивах = " + A.Max());
+                Console.WriteLine("Завершение программы");
+                Environment.Exit(0);
             }
-            else
-            {
-                Console.WriteLine("Общего максимального элемента в массивах нет ");
-            }
-
-            Console.WriteLine("Минимальный элемент массива А = " + A.Min());
-            Console.WriteLine("Минимальный элемент массива B = " + B.Cast<double>().Min());
-
-            if (A.Min() == B.Cast<double>().Min())
-            {
-                Console.WriteLine("Минимальный общий элемент в массивах = " + A.Min());
-            }
-            else
-            {
-                Console.WriteLine("Общего минимального элемента в массивах нет ");
-            }
-
-            Console.WriteLine("Сумма элементов массивов = " + (A.Sum()+ B.Cast<double>().Sum()));
-
-            Console.WriteLine("Общее произведение элементов массивов = " + composAB);
-
-            Console.WriteLine("Cуммa четных элементов массива А = " + sumA);
-
-            Console.WriteLine("Cуммa нечетных столбцов массива В = " + sumB);
         }
     }
 }
